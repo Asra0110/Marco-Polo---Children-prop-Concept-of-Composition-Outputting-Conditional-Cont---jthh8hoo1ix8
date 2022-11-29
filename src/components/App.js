1c7fecaa2d018ce8837b52491686dc35ea831de7
Asra0110 <111082124+Asra0110@users.noreply.github.com>
2022-11-30 00:01:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React, {useState} from 'react'
 import '../styles/App.css';
-const App = () => {
-  const [data, setData] = useState(true)
-  
-  return (
-    <div id="main">
-    <h1 id="marco-polo">{data ? "Marco": "Polo"}</h1>
-<button id = "marco-polo-toggler" onClick={setData(data)}>{ data : "Marco" : "Polo"}</button>
 
-    </div>
-  )
+const Marco = () => {
+
+const [data, setData] = useState(true)
+
+const clickHandler = () => {
+
+setData(!data)
+
 }
 
+return (
 
-export default App;
+<div id="main">
+
+<h1 id="marco-polo">{data ? "Marco" : "Polo"}</h1>
+
+<button id="marco-polo-toggler" onClick={clickHandler}>{data ? "Polo" : "Marco"}</button>
+
+</div>
+
+)
+
+}
+
+export default Marco;
